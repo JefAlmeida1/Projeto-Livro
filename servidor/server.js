e6aae6c9e2cc4c824dcefe7b4251fcd8a83e1dfd
Jef <89365867+JefAlmeida1@users.noreply.github.com>
2021-10-22 16:32:30 -0300

    mudanças

--- servidor/server.js
+++ servidor/server.js
@@ -7,12 +7,12 @@ app.use(bodyParser.urlencoded({extended:true}))
 
 console.log('conectado')
 
-fs.access('C:/Users/Jaf/Documents/Projeto Livro/servidor/node_modules/accepts/data/items.json', fs.constants.F_OK, (error) =>{
+fs.access('C:/Users//Documents/Projeto Livro/servidor/node_modules/accepts/data/items.json', fs.constants.F_OK, (error) =>{
     console.log(error ? 'Arquivo items.json não existe' : '')
 })
 
 const readFile = () =>{
-    const atual = fs.readFileSync('C:/Users/Jaf/Documents/Projeto Livro/servidor/node_modules/accepts/data/items.json', 'utf-8')
+    const atual = fs.readFileSync('C:/Users//Documents/Projeto Livro/servidor/node_modules/accepts/data/items.json', 'utf-8')
     return atual == '' ? [] : JSON.parse(atual)
 }
 
@@ -20,7 +20,7 @@ const writeFile = (arrayElemet) =>{
     const currentData = readFile()
     currentData.push(arrayElemet)
     const updataFile = JSON.stringify(currentData)
-    fs.writeFileSync('C:/Users/Jaf/Documents/Projeto Livro/servidor/node_modules/accepts/data/items.json', updataFile, 'utf-8')
+    fs.writeFileSync('C:/Users//Documents/Projeto Livro/servidor/node_modules/accepts/data/items.json', updataFile, 'utf-8')
 }
 
 app.post('/', (req, resp) =>{
